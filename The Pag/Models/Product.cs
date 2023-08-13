7ad0fd97dac534c5d3335dcf26406ac42054dd9a
sleepyEnthusiast <adilmalik579+burner@gmail.com>
2023-08-13 23:47:52 +1000

    Basic models

---
+++ The Pag/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace The_Pag.Models
+{
+    public class Product
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Genre { get; set; }
+        [Required]
+        public int Subgenre { get; set; }
+        [Required]
+        public DateTime Published { get; set; }
+        [Required]
+        public string LastUpdatedBy { get; set; }
+        [Required]
+        public DateTime LastUpdated { get; set; }
+    }
+}
