57e67d48f4fad892f01306db791b43828f27fa02
sleepyEnthusiast <125939613+sleepyEnthusiast@users.noreply.github.com>
2023-08-21 12:58:52 +1000

    Name Changes to setup database context string

--- The Pag/Program.cs
+++ The Pag/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<StoreDbContext>();
+builder.Services.AddDbContext<StoreDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StoreDbContext")));
 
 var app = builder.Build();
 
