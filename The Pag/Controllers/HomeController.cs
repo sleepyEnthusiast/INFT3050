15ed650d975662577919ab8ea7f7629a32ec0d5f
Jordan Bell <jordan.bell3770@gmail.com>
2023-10-30 22:26:43 +1100

    last UI clean up

--- The Pag/Controllers/HomeController.cs
+++ The Pag/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace The_Pag.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Forgot_Password()
+        {
+            return View();
+        }
     }
 }
